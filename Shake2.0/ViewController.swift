8e005cc494f123e3442eefdecfd19ecc52790522
apadilla <apadilla@andrew.cmu.edu>
2019-05-30 22:34:52 -0500

    Initial Commit

---
+++ Shake2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shake2.0
+//
+//  Created by Antonio Padilla on 5/30/19.
+//  Copyright © 2019 GenOrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
