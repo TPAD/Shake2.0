2dbd33c4e2088a0991e982724c67b243f8969867
Jack Kasbeer <kasbeer@cmu.edu>
2019-06-07 13:29:15 -0500

    Models group created

---
+++ Shake2.0/Models/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  Shake2.0
+//
+//  Created by Jack Kasbeer on 6/7/19.
+//  Copyright © 2019 GenOrg. All rights reserved.
+//
+
+import Foundation
+
+public class Location {
+    
+}
